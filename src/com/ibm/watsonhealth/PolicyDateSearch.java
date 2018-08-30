100a6c831bc3db422a74237c05ecdff604fddc18
c402256 <eric.baxter@ibm.com>
2018-08-30 14:20:12 -0600

    Intial

--- src/com/ibm/watsonhealth/PolicyDateSearch.java
+++ src/com/ibm/watsonhealth/PolicyDateSearch.java
@@ -3,8 +3,6 @@ package com.ibm.watsonhealth;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.EntityResolver;
-
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,8 +14,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -37,7 +37,8 @@ public class PolicyDateSearch {
         try {
 
             List<String> pathList = new ArrayList<String>();
-            try (Stream<Path> paths = Files.walk(Paths.get("C:\\Users\\IBM_ADMIN\\workspace\\siam\\dataprobe-install\\src\\main\\resources\\dataprobe\\int\\openamSecurityPolicy"))) {
+            String envName = "prod";
+            try (Stream<Path> paths = Files.walk(Paths.get("C:\\Users\\IBM_ADMIN\\workspace\\siam\\dataprobe-install\\src\\main\\resources\\dataprobe\\" + envName + "\\openamSecurityPolicy"))) {
                 pathList = paths
                         .filter(p -> p.toString().endsWith(".policies"))
                         .map(path -> Files.isDirectory(path) ? path.toString() + '/' : path.toString())
@@ -54,30 +55,36 @@ public class PolicyDateSearch {
     private static void searchTags(List<String> pathName) {
         try {
             for (String path : pathName) {
-                System.out.println("File Name: " + path);
-                File fXmlFile = new File(path);
+
                 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
                 factory.setNamespaceAware(false);
                 factory.setIgnoringComments(true);
                 factory.setSchema(null);
                 DocumentBuilder builder = factory.newDocumentBuilder();
                 builder.setEntityResolver(new NullEntityResolver());
+                System.out.println("\nFile Name: " + path);
+                File fXmlFile = new File(path);
                 Document doc = builder.parse(fXmlFile);
                 XPathFactory xPathfactory = XPathFactory.newInstance();
                 XPath xpath = xPathfactory.newXPath();
+                XPathExpression creationExpr = xpath.compile("//Policies/Policy[@creationdate]");
                 XPathExpression lastModExpr = xpath.compile("//Policies/Policy[@lastmodifieddate]");
                 XPathExpression policyNameExpr = xpath.compile("//Policies/Policy[@name]");
 
                 NodeList nl = (NodeList) lastModExpr.evaluate(doc, XPathConstants.NODESET);
                 NodeList nodeList = (NodeList) policyNameExpr.evaluate(doc, XPathConstants.NODESET);
+                NodeList nodeLi = (NodeList) creationExpr.evaluate(doc, XPathConstants.NODESET);
 
 
                 for (int i = 0; i < nl.getLength(); i++) {
                     Node lastModNode = nl.item(i);
                     Node nameNode = nodeList.item(i);
+                    Node createDateNode = nodeLi.item(i);
+
+                    String created = createDateNode.getAttributes().getNamedItem("creationdate").getNodeValue();
                     String key = lastModNode.getAttributes().getNamedItem("lastmodifieddate").getNodeValue();
                     String name = nameNode.getAttributes().getNamedItem("name").getNodeValue();
-                    dateTimeNameOutput(name, key);
+                    dateTimeNameOutput(created, name, key);
                 }
             }
         } catch (IOException e) {
@@ -89,14 +96,63 @@ public class PolicyDateSearch {
         }
     }
 
-    private static void dateTimeNameOutput(String name, String key) {
-        Long dateL = Long.valueOf(key);
-        DateTimeFormatter dtFormat = DateTimeFormatter.ofPattern("MMM/dd/yyyy");
-        LocalDate date =
-                Instant.ofEpochMilli(dateL).atZone(ZoneId.systemDefault()).toLocalDate();
-        System.out.println("Policy Name: " + name + "  - Last Modified Date: " + date.format(dtFormat));
+/**
+    public static void searchTags(String path) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(false);
+            factory.setIgnoringComments(true);
+            factory.setSchema(null);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.setEntityResolver(new NullEntityResolver());
+            System.out.println("\nFile Name: " + path);
+            File fXmlFile = new File(path);
+            Document doc = builder.parse(fXmlFile);
+            XPathFactory xPathfactory = XPathFactory.newInstance();
+            XPath xpath = xPathfactory.newXPath();
+            XPathExpression creationExpr = xpath.compile("//Policies/Policy[@creationdate]");
+            XPathExpression lastModExpr = xpath.compile("//Policies/Policy[@lastmodifieddate]");
+            XPathExpression policyNameExpr = xpath.compile("//Policies/Policy[@name]");
+
+            NodeList nl = (NodeList) lastModExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList nodeList = (NodeList) policyNameExpr.evaluate(doc, XPathConstants.NODESET);
+            NodeList nodeLi = (NodeList) creationExpr.evaluate(doc, XPathConstants.NODESET);
 
 
+            for (int i = 0; i < nl.getLength(); i++) {
+                Node lastModNode = nl.item(i);
+                Node nameNode = nodeList.item(i);
+                Node createDateNode = nodeLi.item(i);
+
+                String created = createDateNode.getAttributes().getNamedItem("creationdate").getNodeValue();
+                String key = lastModNode.getAttributes().getNamedItem("lastmodifieddate").getNodeValue();
+                String name = nameNode.getAttributes().getNamedItem("name").getNodeValue();
+                dateTimeNameOutput(created, name, key);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+**/
 
+    private static void dateTimeNameOutput(String creation, String name, String key) {
+        Long dateL = Long.valueOf(key);
+        Long dateC = Long.valueOf(creation);
+        try {
+            DateTimeFormatter dtFormat = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+            LocalDateTime date =
+                    Instant.ofEpochMilli(dateL).atZone(ZoneId.systemDefault()).toLocalDateTime();
+            LocalDateTime dateLC =
+                    Instant.ofEpochMilli(dateC).atZone(ZoneId.systemDefault()).toLocalDateTime();
+            System.out.println("Policy Name: " + name + " Created: " + dateLC.format(dtFormat) +
+                    " - Last Modified Date: " + date.format(dtFormat));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/ibm/watsonhealth/TextSearchWithCollections.java
+++ src/com/ibm/watsonhealth/TextSearchWithCollections.java
@@ -1,27 +1,101 @@
 package com.ibm.watsonhealth;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.*;
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.*;
 
+
 public class TextSearchWithCollections {
     public static void main(String[] args) {
+        addStringsToCollections(args[0], args[1]);
+    }
+
+    private static void addStringsToCollections(String innerFileName, String policyOuterFile) {
         try {
-            addStringsToCollections(args[0], args[1]);
-        } catch (FileNotFoundException e) {
+            HashSet<String> searchList = new HashSet<>() ;
+            List<String> targetList;
+            if(!innerFileName.endsWith("properties")){
+                System.out.println("Agent File is not a property file");
+                System.exit(-1);
+            }
+            if (policyOuterFile.endsWith("xml")) {
+                searchList = addXMLTagsOuterList(policyOuterFile);
+            } else {
+                File outFile = new File(policyOuterFile);
+                final Scanner scanner1 = new Scanner(outFile);
+                while (scanner1.hasNextLine()) {
+                    final String lineFromPolicyFile = scanner1.nextLine();
+                    String searchString = lineFromPolicyFile.substring(lineFromPolicyFile.lastIndexOf("://") + 3);
+                    String finalSearchStr;
+                    if (searchString.contains("/")) {
+                        finalSearchStr = searchString.substring(0, searchString.indexOf("/"));
+                    } else {
+                        finalSearchStr = searchString;
+                    }
+                    searchList.add(finalSearchStr);
+                }
+            }
+                targetList = innerFileList(innerFileName);
+                compareTwoLists(targetList, searchList);
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void addStringsToCollections(String innerFileName, String policyOuterFile) throws FileNotFoundException {
-        File outFile = new File(policyOuterFile);
-        final Scanner scanner1 = new Scanner(outFile);
-        List<String> searchList = new ArrayList<>();
+    private static List<String> innerFileList(String innerFileName) throws FileNotFoundException {
         List<String> targetList = new ArrayList<>();
+        File inFile = new File(innerFileName);
+        final Scanner scanner = new Scanner(inFile);
+        String finalStr;
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+            if (!lineFromFile.startsWith("#") && (lineFromFile.contains("url"))) {
+                String searchString = lineFromFile.substring(lineFromFile.indexOf("://") + 3);
+                if (searchString.contains("/")) {
+                    finalStr = searchString.substring(0, searchString.indexOf("/"));
+                } else {
+                    finalStr = searchString;
+                }
+                targetList.add(finalStr);
+            }
+        }
+        return targetList;
+    }
+
+    private static HashSet<String> addXMLTagsOuterList(String policyOuterFile) {
+        HashSet<String> searchList = new HashSet<>();
         try {
-            while (scanner1.hasNextLine()) {
-                final String lineFromPolicyFile = scanner1.nextLine();
-                String searchString = lineFromPolicyFile.substring(lineFromPolicyFile.lastIndexOf("://") + 3);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(false);
+            factory.setIgnoringComments(true);
+            factory.setSchema(null);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.setEntityResolver(new NullEntityResolver());
+            System.out.println("\nFile Name: " + policyOuterFile);
+            File fXmlFile = new File(policyOuterFile);
+            Document doc = builder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+            XPathFactory xPathfactory = XPathFactory.newInstance();
+            XPath xpath = xPathfactory.newXPath();
+            XPathExpression resourceName = xpath.compile("/Policies/Policy/Rule/ResourceName[@name]");
+
+            NodeList nl = (NodeList) resourceName.evaluate(doc, XPathConstants.NODESET);
+
+            for (int i = 0; i < nl.getLength(); i++) {
+                Node resourceNameNode = nl.item(i);
+
+                String key = resourceNameNode.getAttributes().getNamedItem("name").getNodeValue();
+
+                String searchString = key.substring(key.lastIndexOf("://") + 3);
                 String finalSearchStr;
                 if (searchString.contains("/")) {
                     finalSearchStr = searchString.substring(0, searchString.indexOf("/"));
@@ -29,38 +103,26 @@ public class TextSearchWithCollections {
                     finalSearchStr = searchString;
                 }
                 searchList.add(finalSearchStr);
+//                System.out.println("this is the value: " +key);
             }
-            File inFile = new File(innerFileName);
-            final Scanner scanner = new Scanner(inFile);
-            String finalStr;
-            while (scanner.hasNextLine()) {
-                final String lineFromFile = scanner.nextLine();
-                if (!lineFromFile.startsWith("#") && (lineFromFile.contains("url"))) {
-                    String searchString = lineFromFile.substring(lineFromFile.indexOf("://") + 3);
-                    if (searchString.contains("/")) {
-                        finalStr = searchString.substring(0, searchString.indexOf("/"));
-                    } else {
-                        finalStr = searchString;
-                    }
-                    targetList.add(finalStr);
-                }
-            }
-            compareTwoLists(targetList, searchList);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return searchList;
     }
 
-    private static void compareTwoLists(List<String> agentFile, List<String> policyList) {
+    private static void compareTwoLists(List<String> agentFile, HashSet<String> policyList) {
         int count = 1;
         int notCount = 1;
         try {
             for (String agentStr : policyList) {
                 if (agentFile.contains(agentStr)) {
-                    System.out.println( count + " Found " + agentStr + "    ");
+                    System.out.println(count + " Found " + agentStr + "    ");
                     count++;
-                }else{
-                    System.out.println(notCount +" ** NOT Found in agent.property file "+ agentStr );
+                } else {
+                    System.out.println(notCount + " ** NOT Found in agent.property file " + agentStr);
                     notCount++;
                 }
             }
